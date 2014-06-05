8d5e5f38ad34f41a59dd9c0b8d57eebf579044bd
glandu2 <glandu2epvp@gmail.com>
2014-06-05 02:03:10 +0200

    Add SkillTree for epic 5.2

---
+++ Databases/SkillTree52Database.cpp
@@ -0,0 +1,42 @@
+#include "DataType.h"
+#include "ExportDLL.h"
+#include "SpecialDatabaseRules.h"
+#include <stdlib.h>
+#include <string.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//Fixme: special behavior for this (double for)
+
+static FieldDescriptor df[] =
+	{{1, TYPE_INT32 | TYPE_FLAG_SORT, "job_id"},
+	 {1, TYPE_INT32, "skill_id"},
+	 {1, TYPE_INT32, "min_skill_lv"},
+	 {1, TYPE_INT32, "max_skill_lv"},
+	 {1, TYPE_INT32, "lv"},
+	 {1, TYPE_INT32, "job_lv"},
+	 {1, TYPE_FLOAT32, "jp_ratio"},
+	 {1, TYPE_INT32, "need_skill_id_1"},
+	 {1, TYPE_INT32, "need_skill_id_2"},
+	 {1, TYPE_INT32, "need_skill_id_3"},
+	 {1, TYPE_INT32, "need_skill_lv_1"},
+	 {1, TYPE_INT32, "need_skill_lv_2"},
+	 {1, TYPE_INT32, "need_skill_lv_3"}};
+
+#pragma comment(linker, "/EXPORT:registerDBStructure=_registerDBStructure@8")
+void EDATABASEDLL DLLCALLCONV registerDBStructure(FieldDescriptor **dfmPtr, int *sizePtr) {
+	*dfmPtr = df;
+	*sizePtr = sizeof(df) / sizeof(FieldDescriptor);
+}
+
+#pragma comment(linker, "/EXPORT:getSpecialCaseID=_getSpecialCaseID@0")
+int EDATABASEDLL DLLCALLCONV getSpecialCaseID() {
+	return SPECIALCASE_DOUBLEFORRDB;
+}
+
+#ifdef __cplusplus
+}
+#endif
+
