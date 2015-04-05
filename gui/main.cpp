e590701778eff69d159ee2c0e6f1f3c1bb3acc4f
glandu2 <glandu2epvp@gmail.com>
2015-04-06 00:52:08 +0200

    Update version to 2.8.0

--- gui/main.cpp
+++ gui/main.cpp
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
 	a.installTranslator(&translationFile);
 
 	a.setApplicationName("RappelzRDBTool ~ Glandu2");
-	a.setApplicationVersion("2.7.0");
+	a.setApplicationVersion("2.8.0");
 
 	Maingui mainGui;
 
