17710c061dec01ac3cd8a2f987b2fae1f97f0918
glandu2 <glandu2epvp@gmail.com>
2017-08-27 01:47:29 +0200

    Add State31Database and String31Database in dll format

---
+++ Databases/String31Database.cpp
@@ -0,0 +1,26 @@
+#include "IRowManipulator.h"
+#include "DataType.h"
+#include "ExportDLL.h"
+#include <stdlib.h>
+#include <string.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+static FieldDescriptor df[] =
+	{{1, TYPE_INT32, "code"},
+	 {1, TYPE_INT32, "group_id"},
+	 {2048, TYPE_CHAR, "name"},
+	 {2048, TYPE_CHAR, "value"}};
+
+#pragma comment(linker, "/EXPORT:registerDBStructure=_registerDBStructure@8")
+void EDATABASEDLL DLLCALLCONV registerDBStructure(FieldDescriptor **dfmPtr, int *sizePtr) {
+	*dfmPtr = df;
+	*sizePtr = sizeof(df) / sizeof(FieldDescriptor);
+}
+
+#ifdef __cplusplus
+}
+#endif
+
