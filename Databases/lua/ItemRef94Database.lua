8926184185cb8615f3b3a8602bd499c738c386c2
glandu2 <glandu2epvp@gmail.com>
2017-10-31 22:17:17 +0100

    Fix index row in ItemRef94Database.lua

--- Databases/lua/ItemRef94Database.lua
+++ Databases/lua/ItemRef94Database.lua
@@ -1,3 +1,6 @@
+-- By Gangor
+-- https://www.elitepvpers.com/forum/rappelz-private-server/2769975-release-rappelz-rdb-tool-tabs-21.html#post36438074
+
 rdb.fields = {
 	{"id", TYPE_INT16 | TYPE_RDBIGNORE | TYPE_FLAG_SORT},
 	{"string", TYPE_CHAR, 52},
@@ -5,8 +8,10 @@ rdb.fields = {
 
 rdb.defaultFileName = "db_item.ref"
 
+rdb.defaultTableName = "ItemReferenceResource"
+
 rdb.convertData = function (dst, mode, row, rowNum)
 	if mode == DCT_Read and dst == DF_RDB then
-		row.id = rowNum + 1
+		row.id = rowNum
 	end
 end
